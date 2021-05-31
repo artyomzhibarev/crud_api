a4d2abd43b9ecd91fe884bae50243bac6a5202d5
artyomzhibarev <artyom.zhibarev@yandex.ru>
2021-05-31 05:03:40 +0300

    tests

--- todo/urls.py
+++ todo/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 # router = DefaultRouter()
-# router.register('', views.NoteViewSet, basename='todos')
+# router.register(r'notes', views.NoteList, basename='notes')
+# router.register(r'note', views.NoteDetail, basename='note')
 # urlpatterns = router.urls
 
-
 urlpatterns = [
-    path('<username>/', views.NoteListAPIView.as_view(), name='notes'),
-    path('<username>/<int:id>/', views.NoteDetailAPIView.as_view(), name='note')
+    path(r'<username>/', csrf_exempt(views.NoteListAPIView.as_view()), name='notes'),
+    path(r'<username>/<int:note_id>/', csrf_exempt(views.NoteDetailAPIView.as_view()), name='note')
 ]
+
+# urlpatterns = [
+#     path(r'notes/', csrf_exempt(views.NoteListAPIView.as_view()), name='notes'),
+#     path(r'<int:id>/', csrf_exempt(views.NoteDetailAPIView.as_view()), name='note')
+# ]
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,48 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, mixins, status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, GenericViewSet
+
 from .models import Note
 from .permissions import IsOwner
 from .serializers import NoteSerializer
 
 
-# class NoteViewSet(viewsets.ModelViewSet):
+# class NoteViewSet(viewsets.ReadOnlyModelViewSet):
 #     queryset = Note.objects.all().order_by('create_at')
 #     serializer_class = NoteSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+#     permission_classes = [permissions.IsAuthenticated, IsOwner]
+#     lookup_field = 'id'
+#     slug_field = 'username'
+#
+#     def get_queryset(self):
+#         return Note.objects.filter(author=self.request.user)
+#
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+#
+#     def get_username(self):
+#         user = self.request.user
+#         return user.username
+
+
+# class NoteList(ReadOnlyModelViewSet):
+#     serializer_class = NoteSerializer
+#     permission_classes = [permissions.IsAuthenticated, ]
+#
+#     def get_queryset(self):
+#         return Note.objects.filter(author=self.request.user)
+#
+#
+# class NoteDetail(mixins.CreateModelMixin,
+#                  mixins.RetrieveModelMixin,
+#                  mixins.UpdateModelMixin,
+#                  mixins.DestroyModelMixin,
+#                  GenericViewSet):
+#     serializer_class = NoteSerializer
+#     queryset = Note.objects.all().order_by('create_at')
+#     permission_classes = (permissions.IsAuthenticated, IsOwner)
+#     lookup_field = 'id'
 #
 #     def get_queryset(self):
 #         return Note.objects.filter(author=self.request.user)
@@ -18,25 +52,37 @@ from .serializers import NoteSerializer
 
 
 class NoteListAPIView(ListCreateAPIView):
+    # queryset = Note.objects.all().order_by('create_at')
     serializer_class = NoteSerializer
-    queryset = Note.objects.all().order_by('create_at')
-    permission_classes = [permissions.IsAuthenticated, ]
+    permission_classes = (permissions.IsAuthenticated, )
+    # permission_classes = (IsOwner, )
 
     def get_queryset(self):
-        return Note.objects.filter(author=self.request.user)
+        return Note.objects.filter(author=self.request.user).order_by('-create_at')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        if request.user.is_authenticated:
+            notes = self.get_serializer(queryset, many=True)
+            print(notes.data)
+            return Response(notes.data)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
 
 
 class NoteDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = NoteSerializer
-    queryset = Note.objects.all().order_by('create_at')
-    permission_classes = (permissions.IsAuthenticated, IsOwner)
-    lookup_field = 'id'
+    permission_classes = (permissions.IsAuthenticated, )
+    # permission_classes = (IsOwner, )
+    lookup_field = 'note_id'
 
     def get_queryset(self):
         return Note.objects.filter(author=self.request.user)
 
-    def perform_create(self, serializer):
+    def perform_update(self, serializer):
         serializer.save(author=self.request.user)
+
